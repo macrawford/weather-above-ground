8ecf252cb5e1a745df0cb1aa8da82129b38a2071
Matthew Crawford <matthew.crawford92@gmail.com>
2021-06-13 18:44:22 -0700

    Created API call to weather data site and mapped weather by hour

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {key} from '../../apikey.js';
+import axios from 'axios';
 
 const App = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+  const fetchData = () => {
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?units=imperial&zip=98122&appid=' + key)
+    .then((response) => {
+      console.log('response.data: ', response.data)
+      setCity(response.data.city.name)
+      setData(response.data.list)
+    })
+  };
+
   return (
-    <div>React working!</div>
+    <div>
+      <div>Weather for {city}</div>
+      {weatherData.map((data) => {
+        return (
+          <div>
+            <div>{data.main.temp}</div>
+            <div>{data.dt_txt}</div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
