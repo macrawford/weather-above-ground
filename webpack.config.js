7b22b591591b8ce9dacb3e68f92e95bfeb6e0307
Matthew Crawford <matthew.crawford92@gmail.com>
2021-06-13 17:39:27 -0700

    Installed dependencies for React project with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
